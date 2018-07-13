33a7fb2b1683a7f42bcb0057d05a7c0eecb1522d
satty28 <bsathiyan88@gmail.com>
2018-07-13 05:52:26 +0000

    change 2

--- WebApplication1aspdotnet/Startup.cs
+++ WebApplication1aspdotnet/Startup.cs
@@ -27,7 +27,7 @@ namespace WebApplication1aspdotnet
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World2!");
             });
         }
     }
